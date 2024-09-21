d74f478f411f55d1bda54f0f75bf751b141c6e42
Jezreel Franklin <mavyjayann@gmail.com>
2024-09-21 18:45:04 +0100

    Add Header component

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <div>
+    <div class="container__header">
       <h1>Rave</h1>
       <h1>Index</h1>
     </div>
@@ -10,4 +10,22 @@
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+.container__header {
+  font-family: Danfo;
+  font-size: 10vmin;
+  justify-content: space-between;
+  display: flex;
+  width: 100%;
+
+  h1 {
+    font-family: inherit;
+    font-variation-settings: "ELSH" 50;
+  }
+}
+</style>
