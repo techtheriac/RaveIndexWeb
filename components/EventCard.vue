e0b85408a025bce529bd5d0fcf69580d90d190b4
Jezreel Franklin <mavyjayann@gmail.com>
2024-09-21 18:44:47 +0100

    scaffold event card

---
+++ components/EventCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>
+      {{ title }}
+    </h2>
+    <span>
+      {{ date }}
+    </span>
+    <span v-if="meta">
+      {{ meta }}
+    </span>
+    <img :src="art" :alt="title" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Url } from "~/types";
+
+const props = defineProps<{
+  date: string;
+  title: string;
+  art: string | Url;
+  meta?: string;
+}>();
+</script>
+
+<style scoped></style>
